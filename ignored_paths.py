2d1fbae9f499b038cc695b9c8e89bafee7a793a8
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2016-02-07 12:54:45 +0000

    Ignore danfysik for now

--- ignored_paths.py
+++ ignored_paths.py
@@ -1,3 +1,3 @@
 # If this matches part of a directory path, it is not checked
 def ignored_paths():
-    return [ "\\optics\\", "\\areaDetector\\", "\\EPICS_V4\\", "\\CALab\\" ]
+    return [ "\\optics\\", "\\areaDetector\\", "\\EPICS_V4\\", "\\CALab\\", "\\danfysikMps8000\\" ]
